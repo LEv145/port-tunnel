dc03f6e73d88ab03116d170764b843d6071253e2
levxx <example@mail.com>
2026-06-24 19:46:30 +0200

    Поправлены импорты

--- src/common/port_tunnel_common/mixins/protocol_transmitter.py
+++ src/common/port_tunnel_common/mixins/protocol_transmitter.py
@@ -1,19 +1,19 @@
 import asyncio
 
-from port_tunnel_transmitters import ABCTransmitter
+from port_tunnel_common.codecs import ABCMessageCodec
 from port_tunnel_protocol.codec import ControlMessage, parse_control_message, serialize_control_message
 from port_tunnel_protocol.messages import ControlMessageBase
 
 
 class ProtocolTransmitterMixin:
-    _transmitter: ABCTransmitter
+    _codec: ABCMessageCodec
 
     async def _read_control_message(
         self,
         reader: asyncio.StreamReader,
     ) -> ControlMessage:
         """Прочитать и проверить одно сообщение управляющего протокола."""
-        payload = await self._transmitter.read_json(reader)
+        payload = await self._codec.read_json(reader)
         return parse_control_message(payload)
 
 
@@ -24,7 +24,7 @@ class ProtocolTransmitterMixin:
     ) -> None:
         """Сериализовать и отправить управляющее сообщение."""
         payload = serialize_control_message(message)
-        await self._transmitter.send_json(
+        await self._codec.send_json(
             writer,
             payload,
         )
